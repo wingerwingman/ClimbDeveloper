cff3ac6799badb7ea442ca986df5095ace69569e
Justin Winger <justinwinger4@gmail.com>
2020-12-03 13:58:37 -0700

    Setup validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, :email, :dob, :user_name, :location, :gender, presence: true 
+  validates :email, :user_name, uniqueness: true 
+  validates :password, confirmation: true 
 end
