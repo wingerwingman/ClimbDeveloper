9ed29f5f01b94cd78c54d3b098675ba964a2f084
Justin Winger <justinwinger4@gmail.com>
2020-12-03 11:38:04 -0700

    area creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'welcome/home'
-  get 'welcome/about'
+  resources :areas
+  root to: 'welcome#home'
+  get 'about', to: 'welcome#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
